69b6fa85b314cbbce95430c7021ed8fb055a75ae
Tony Liang <chunqingliang@gmail.com>
2023-05-27 18:43:46 -0700

    add insctruction to install

---
+++ README.md
@@ -0,0 +1,13 @@
+# Vina Usage
+
+You should clone the AutoDock-Vina [repo]() and use its example to learn how to use the tool.
+
+## Software Requirements
+
+Note, its [official docs installation guide](https://autodock-vina.readthedocs.io/en/latest/installation.html) is out-to-date. I have provides two scripts under `./src` that provides installation steps to get these stacks.
+
+- `Python==3.9.7`
+- `conda==23.5.0`
+- `AutoDock Vina v1.2.3`
+
+
---
+++ src/install_vina_conda_linux_x86.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+chmod +x $0
+# Script to install autodock vina in Linux x86-64
+function man {
+cat << EOF
+
+Downloads vina binary and conda env to Linux x86-64
+
+Usage: bash $0
+
+EOF
+}
+
+
+
+############## Binary installation
+# Make directory to store binary
+BIN_DIR="${HOME}/.local/bin"
+VINA="${BIN_DIR}/vina"
+if [ ! -d ${BIN_DIR} ]
+then
+    echo 'Creating dir not'
+    mkdir -pv ${BIN_DIR}    
+fi
+echo 'Dir created, proceeds'
+echo 'Start to download, wait a few secs'
+if [ ! -f $VINA ]
+then 
+    URL="https://github.com/ccsb-scripps/AutoDock-Vina/releases/download/v1.2.3/vina_1.2.3_linux_x86_64"
+    wget -qO- ${URL} > $VINA
+    chmod +x $VINA
+fi
+echo 'Downloaded autodock vina binary'
+############### Conda installation
+CONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-py310_23.3.1-0-Linux-x86_64.sh"
+if ! command -v conda &> /dev/null
+then
+    echo 'Downloading conda quietly now'  
+    wget ${CONDA_URL} -qO ~/miniconda.sh
+    bash ~/miniconda.sh -b -f -p $HOME/miniconda
+    # Initialize PATH
+    source ~/miniconda/bin/activate && conda init bash
+fi
+echo 'Conda downloaded'
+############### clean up
+rm -f miniconda.sh
+###############
+echo 'Done'
