bfedcee79a9605163e88c016e3940e4871dc44a1
Tony Liang <chunqingliang@gmail.com>
2023-05-27 18:50:00 -0700

    update install message

--- src/install_vina_conda_linux_x86.sh
+++ src/install_vina_conda_linux_x86.sh
@@ -15,15 +15,16 @@ EOF
 
 ############## Binary installation
 # Make directory to store binary
-BIN_DIR="${HOME}/.local/bin"
+BIN_DIR="$HOME/.local/bin"
 VINA="${BIN_DIR}/vina"
+
 if [ ! -d ${BIN_DIR} ]
 then
-    echo 'Creating dir not'
-    mkdir -pv ${BIN_DIR}    
+    echo Creating dir now
+    mkdir -pv ${BIN_DIR}
 fi
-echo 'Dir created, proceeds'
-echo 'Start to download, wait a few secs'
+echo Dir created, proceeds
+echo Start to download, wait a few secs
 if [ ! -f $VINA ]
 then 
     URL="https://github.com/ccsb-scripps/AutoDock-Vina/releases/download/v1.2.3/vina_1.2.3_linux_x86_64"
@@ -35,14 +36,14 @@ echo 'Downloaded autodock vina binary'
 CONDA_URL="https://repo.anaconda.com/miniconda/Miniconda3-py310_23.3.1-0-Linux-x86_64.sh"
 if ! command -v conda &> /dev/null
 then
-    echo 'Downloading conda quietly now'  
+    echo Downloading conda quietly now
     wget ${CONDA_URL} -qO ~/miniconda.sh
     bash ~/miniconda.sh -b -f -p $HOME/miniconda
     # Initialize PATH
     source ~/miniconda/bin/activate && conda init bash
 fi
-echo 'Conda downloaded'
+echo Conda downloaded
 ############### clean up
 rm -f miniconda.sh
 ###############
-echo 'Done'
+echo Done, you should add $BIN_DIR to your PATH
